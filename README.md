4817c50093e2c9b7cffcec77162a9817bb017a4b
rash <rash@rash.codes>
2022-03-29 20:52:38 +0200

    add own no-parsing-error rule

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # eslint-plugin-vue-pug
 
+Extends [eslint-plugin-vue](https://eslint.vuejs.org/) to support pug templates
+
+## Installation
+
+```sh
+npm install --save-dev eslint eslint-plugin-vue eslint-plugin-vue-pug
+```
+
+## Usage
+
+Most [eslint-plugin-vue](https://eslint.vuejs.org/) work out of the box with just configuring 
+
+If you are changing `parserOptions` yourself, add `templateTokenizer: { pug: 'vue-eslint-parser-template-tokenizer-pug' }` to `parserOptions`.
+
 ## Notes
 
 ### Comment Directive
---
+++ tests/rules/no-parsing-error.js
@@ -0,0 +1,51 @@
+const RuleTester = require('eslint').RuleTester
+const rule = require('../../lib/rules/no-parsing-error')
+
+const tester = new RuleTester({
+  parser: require.resolve('vue-eslint-parser'),
+  parserOptions: {
+    ecmaVersion: 2015,
+    templateTokenizer: { pug: 'vue-eslint-parser-template-tokenizer-pug' }
+  }
+})
+
+// TODO add more test cases
+tester.run('no-parsing-error', rule, {
+  valid: [
+    {
+      filename: 'test.vue',
+      code: ''
+    },
+    {
+      filename: 'test.vue',
+      code: `<template lang="pug">div(:a="3") {{ 3 + 4 }}</template>`
+    }
+    // {
+    //   code: `<template lang="pug"></template>`,
+    //   options: [{ '': false }]
+    // },
+  ],
+  invalid: [
+    // NO_END_BRACKET
+    {
+      filename: 'test.vue',
+      code: `<template lang="pug">div(a</template>`,
+      errors: [
+        'Parsing error: The end of the string reached with no closing bracket ) found.'
+      ]
+    },
+    // UNEXPECTED_TEXT
+    {
+      filename: 'test.vue',
+      code: `<template lang="pug">div(a))</template>`,
+      errors: ['Parsing error: unexpected text ")"']
+    }
+    // {
+    //   filename: 'test.vue',
+    //   code: `<template lang="pug">
+
+    // </template>`,
+    //   errors: ['Parsing error: ']
+    // },
+  ]
+})
